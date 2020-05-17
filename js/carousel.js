38bf797717c931921df789dcd85b4eb4c6a5f60c
andresfeliper3 <andresfeliper.2001@gmail.com>
2020-05-17 13:41:59 -0500

    esfera arreglada para pc, falta movil

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+//Carousel options
+$('#carousel1').carousel({
+    interval: 4000  
+  })
+
+//Clicks on options
+$('#stage1').click(function() 
+{
+    $('#carousel1').carousel(0);
+});
+$('#stage2').click(function() 
+{
+    $('#carousel1').carousel(1);
+});
+$('#stage3').click(function() 
+{
+    $('#carousel1').carousel(2);
+});
+$('#stage4').click(function() 
+{
+    $('#carousel1').carousel(3);
+});
+
